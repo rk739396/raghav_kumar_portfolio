31a73cd804b0f543547d483d4e325b80433b6d4f
rk739396 <51133126+rk739396@users.noreply.github.com>
2025-05-15 23:34:12 +0530

    Update main.js
    sdjklf;

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-<script>
-  document.getElementById('experienceLink').addEventListener('click', function() {
-    const experienceSection = document.querySelector('.text-gray-100');  // You can use a class or any other selector to target the experience section
-    experienceSection.scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-</script>
+
